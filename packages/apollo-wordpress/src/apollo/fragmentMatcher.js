ae77595c11a61f56442dcddc8e33e8d17d11eecf
Scott Taylor <scott.c.taylor@mac.com>
2017-10-14 16:51:33 -0400

    ES7 cleanup

--- packages/apollo-wordpress/src/apollo/fragmentMatcher.js
+++ packages/apollo-wordpress/src/apollo/fragmentMatcher.js
@@ -2,9 +2,7 @@ import { IntrospectionFragmentMatcher } from 'react-apollo';
 import __schema from '../../tools/fragmentMatcher.json';
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData: {
-    __schema,
-  },
+  introspectionQueryResultData: __schema,
 });
 
 export default fragmentMatcher;
