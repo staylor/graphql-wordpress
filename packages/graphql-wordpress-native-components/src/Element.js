2f65f789ac3a3ea78824315c9014fc8d650c1096
Scott Taylor <scott.c.taylor@mac.com>
2017-10-14 21:49:54 -0400

    create native component repo

---
+++ packages/graphql-wordpress-native-components/src/Element.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Text, Image, WebView, Dimensions, Linking } from 'react-native';
+
+/* eslint-disable react/prop-types */
+
+export default ({ node, children, parent = null }) => {
+  const props = { style: {} };
+  const attrs = (node.attributes || []).reduce((memo, { name, value }) => {
+    memo[name] = value;
+    return memo;
+  }, {});
+
+  const elementChildren = children;
+  let responsiveWidth = null;
+  let responsiveHeight = null;
+  let elementWidth = null;
+  let elementHeight = null;
+
+  let tag = Text;
+  switch (node.tagName) {
+    case 'iframe':
+      tag = WebView;
+
+      responsiveWidth = Dimensions.get('window').width;
+      responsiveHeight = parseInt(attrs.height || 80, 10);
+      props.style = {
+        flex: -1,
+        width: responsiveWidth,
+        height: responsiveHeight,
+        marginBottom: 20,
+      };
+      props.source = { uri: attrs.src };
+      break;
+
+    case 'img':
+      tag = Image;
+
+      responsiveWidth = Dimensions.get('window').width;
+      elementWidth = attrs.width || responsiveWidth;
+      elementHeight = attrs.height || responsiveWidth * 0.75;
+      responsiveHeight = responsiveWidth * elementHeight / elementWidth;
+
+      props.style = {
+        flex: -1,
+        width: responsiveWidth,
+        height: responsiveHeight,
+      };
+      props.source = { uri: attrs.src };
+      break;
+
+    case 'blockquote':
+      props.style = {
+        paddingLeft: 20,
+        paddingRight: 40,
+        paddingBottom: 20,
+        fontSize: 14,
+      };
+      break;
+
+    case 'em':
+      props.style = {
+        textDecorationLine: 'underline',
+      };
+      break;
+
+    case 'h1':
+    case 'h2':
+    case 'h3':
+    case 'h4':
+    case 'h5':
+    case 'h6':
+      props.style.paddingLeft = 10;
+      props.style.paddingRight = 10;
+    // eslint-disable-next-line
+    case "strong":
+      props.style.fontWeight = 'bold';
+      break;
+
+    case 'a':
+      props.style = { color: '#e50082' };
+      props.onPress = () => Linking.openURL(attrs.href);
+      break;
+
+    case 'p':
+      if (parent && parent.tagName === 'blockquote') {
+        return elementChildren;
+      }
+      props.style = {
+        marginBottom: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+      };
+      break;
+    default:
+      break;
+  }
+
+  return React.createElement(tag, props, elementChildren);
+};
