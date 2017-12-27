ec089464ce51e47db383062bf60f649b07207d25
Scott Taylor <scott.c.taylor@mac.com>
2017-12-27 15:06:14 -0500

    fix pagination matrix for shows

--- packages/draft/src/server/graphql/models/Show.js
+++ packages/draft/src/server/graphql/models/Show.js
@@ -7,6 +7,23 @@ export default class Show extends Model {
     this.collection = context.db.collection('show');
   }
 
+  count({ latest = null, artist = null, venue = null, search = null }) {
+    const criteria = {};
+    if (latest) {
+      criteria.date = { $gte: Date.now() };
+    }
+    if (artist) {
+      criteria.artist = artist;
+    }
+    if (venue) {
+      criteria.venue = venue;
+    }
+    if (search) {
+      criteria.$text = { $search: search };
+    }
+    return this.collection.find(criteria).count();
+  }
+
   async all({
     limit = 10,
     offset = 0,
